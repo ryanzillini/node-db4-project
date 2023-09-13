b6f8fe905919f1d7398d0991a033abe2f2dd1dac
Ryan Zillini <nyrtrading134@gmail.com>
2023-09-13 12:54:04 -0400

    migrations complete

--- data/migrations/20230913161223_initial-migration.js
+++ data/migrations/20230913161223_initial-migration.js
@@ -10,7 +10,7 @@ exports.up = async function (knex) {
       tbl.string("ingredient_unit", 50);
     })
     .createTable("steps", (tbl) => {
-      tbl.increments('step_id');
+      tbl.increments("step_id");
       tbl.string("step_text", 200).notNullable();
       tbl.integer("step_number").notNullable();
       tbl
@@ -23,8 +23,16 @@ exports.up = async function (knex) {
         .onUpdate("RESTRICT");
     })
     .createTable("step_ingredients", (tbl) => {
-      tbl.increments()
-      tbl.
+      tbl.increments("step_ingredients_id");
+      tbl.float("quantity").notNullable();
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
